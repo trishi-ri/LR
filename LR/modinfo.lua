02261dfda31e77a0340f73e3a0436cfb9f54b182
trishi-ri <trishi-ri@ya.ru>
2017-03-14 10:20:57 +0300

    new version

--- LR/modinfo.lua
+++ LR/modinfo.lua
@@ -1,6 +1,6 @@
 -- This information tells other players more about the mod
 name = "Llorar & Ri"
-version = "1.13b"
+version = "1.14"
 description = "He is our Last Hope. She is just a red panda.\n\nVersion: "..version
 author = "Ri-16"
 forumthread = ""
